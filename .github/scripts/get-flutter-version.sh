637afcc5a4ac18295b3c642cba7597918c718366
code <heshamkoptan@gmail.com>
2026-09-09 20:10:06 +0300

    sc

--- .github/scripts/get-flutter-version.sh
+++ .github/scripts/get-flutter-version.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 set -e
-RAW_VER=$(grep -A 5 "environment:" pubspec.yaml | grep "flutter:" || true)
-FLUTTER_VER=$(echo "$RAW_VER" | sed -E 's/.*flutter:[[:space:]]*"?\'?//; s/["'\''^>=].*//' | tr -d '[:space:]')
-
-# في حال عدم العثور على إصدار نستخدم stable
+FLUTTER_VER=$(awk '/environment:/,/dependencies:/' pubspec.yaml | awk '/flutter:/ {print $2}' | tr -cd '0-9.')
 FINAL_VER="${FLUTTER_VER:-stable}"
-
 echo "Extracted Flutter Version: $FINAL_VER"
-
-# حفظ النتيجة في GITHUB_OUTPUT و GITHUB_ENV للتوافقيّة
 if [ -n "$GITHUB_OUTPUT" ]; then
   echo "version=$FINAL_VER" >> "$GITHUB_OUTPUT"
 fi
