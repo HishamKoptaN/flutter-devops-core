69b399d124c6a91de428d9ab5f5bcd5cc2caf06f
code <heshamkoptan@gmail.com>
2026-09-20 20:00:31 +0300

    sc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'config/env_config.dart';
 import 'firebase_debug_screen.dart';
 
